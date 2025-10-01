4aa44a7025aaea04721de63b7a6a2326eeb177e3
Aritro Sarkar <74608503+asarkar8400@users.noreply.github.com>
2025-10-01 10:45:08 -0400

    Create unpipelined_mac.sv

---
+++ Project/Part1/unpipelined_mac.sv
@@ -0,0 +1,27 @@
+module unpipelined_mac #(
+parameter INW = 16,
+parameter OUTW = 64
+)(
+input signed [INW-1:0] input0, input1, init_value,
+output logic signed [OUTW-1:0] out,
+input clk, reset, init_acc, input_valid
+);
+
+logic signed [OUTW-1:0] product;
+
+always_comb begin
+    product = input0 * input1; //first multiplier
+end
+
+    always_ff @(posedge clk) begin
+        if(reset) begin
+            out <= 0; //synch reset
+        end else if(init_acc) begin
+            out <= init_value; // initialize a value
+        end else if(input_valid) begin
+            out <= product + out; // accumulate
+        end else begin
+            out <= out; // do nothing
+        end      
+    end
+endmodule
